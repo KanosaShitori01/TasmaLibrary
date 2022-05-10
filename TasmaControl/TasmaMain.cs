984cb31c3f39e7d86dfd0e969e8673a8ca63cda2
KanosaShitori01 <tranchauqn9@gmail.com>
2022-05-10 23:22:15 +0700

    OKE

---
+++ TasmaControl/TasmaMain.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace TasmaControl
+{
+    public class TasmaMain
+    {
+        public static string StrangeDate(string date)
+        {
+            string[] dates = date.Split('/');
+            Array.Reverse(dates);
+            return String.Join("-", dates);
+        }
+        public static DataTable LietKeTuDo(string lenh, SqlConnection ketnoi)
+        {
+            DataTable dt = new DataTable();
+            SqlDataAdapter adap = new SqlDataAdapter(lenh, ketnoi);
+            adap.Fill(dt);
+            return dt;
+        }
+        public static DataTable LietKeDuLieu(string tentable, SqlConnection ketnoi)
+        {
+            string query = "SELECT * FROM " + tentable;
+            DataTable dt = new DataTable();
+            SqlDataAdapter adap = new SqlDataAdapter(query, ketnoi);
+            adap.Fill(dt);
+            return dt;
+        }
+        public static DataTable LietKeDuLieu(string tentable, string tencotthamchieu, 
+            object giatrithamchieu, SqlConnection ketnoi)
+        {
+            try { Convert.ToInt32(giatrithamchieu); } catch { giatrithamchieu = "'" + giatrithamchieu + "'"; }
+            string query = String.Format("SELECT * FROM {0} WHERE {1}={2}", tentable, 
+                tencotthamchieu, giatrithamchieu);
+            DataTable dt = new DataTable();
+            SqlDataAdapter adap = new SqlDataAdapter(query, ketnoi);
+            adap.Fill(dt);
+            return dt;
+        }
+        public static bool ThemDuLieu(string tentable, Dictionary<string, object> dlcanthem, SqlConnection ketnoi)
+        {
+            bool result = true;
+            string strKey = "";
+            string strValue = "";
+            foreach(var dl in dlcanthem)
+            {
+                strKey += dl.Key + ",";
+                try {
+                    strValue += String.Format("{0},", Convert.ToInt32(dl.Value));
+                }
+                catch {
+                    strValue += String.Format("N'{0}',", dl.Value);
+                }
+            }
+            strKey = strKey.Substring(0, strKey.Length - 1);
+            strValue = strValue.Substring(0, strValue.Length - 1);
+            string query = String.Format("INSERT INTO {0}({1}) VALUES({2})", tentable, strKey, strValue);
+            SqlCommand cmd = new SqlCommand(query, ketnoi);
+            cmd.Connection.Open();
+            try { result = (cmd.ExecuteNonQuery() > 0); }
+            catch { result = false; }
+            cmd.Connection.Close();
+            return result;
+        }
+        public static bool SuaDuLieu(string tentable, Dictionary<string, object> dlcansua, 
+            string tencotthamchieu, object giatrithamchieu,
+            SqlConnection ketnoi)
+        {
+            bool result;
+            try { Convert.ToInt32(giatrithamchieu); } catch { giatrithamchieu = "'" + giatrithamchieu + "'"; }
+            string strRes = "";
+            foreach (var dl in dlcansua)
+            {
+                try
+                {
+                    strRes += String.Format("{0}={1},", dl.Key, Convert.ToInt32(dl.Value));
+                }
+                catch
+                {
+                    strRes += String.Format("{0}=N'{1}',", dl.Key, dl.Value);
+                }
+            }
+            
+            strRes = strRes.Substring(0, strRes.Length - 1);
+            string query = String.Format("UPDATE {0} SET {1} WHERE {2}={3}", tentable, strRes, 
+                tencotthamchieu, giatrithamchieu);
+            SqlCommand cmd = new SqlCommand(query, ketnoi);
+            cmd.Connection.Open();
+            try { result = (cmd.ExecuteNonQuery() > 0); }
+            catch { result = false; }
+            cmd.Connection.Close();
+            return result;
+        }
+        public static bool XoaDuLieu(string tentable, string tencotthamchieu, object giatrithamchieu, SqlConnection ketnoi)
+        {
+            bool result;
+            try { Convert.ToInt32(giatrithamchieu); } catch { giatrithamchieu = "'" + giatrithamchieu + "'"; }
+            string query = String.Format("DELETE FROM {0} WHERE {1}={2}", tentable,
+                tencotthamchieu, giatrithamchieu);
+            SqlCommand cmd = new SqlCommand(query, ketnoi);
+            cmd.Connection.Open();
+            try { result = (cmd.ExecuteNonQuery() > 0); }
+            catch { result = false; }
+            cmd.Connection.Close();
+            return result;
+        }
+        public static SqlConnection ketnoi(string tenserver, string tencsdl)
+        {
+            string query = String.Format(@"Server={0};Database={1};Integrated Security=true", tenserver, tencsdl);
+            SqlConnection conn = new SqlConnection(query);
+            return conn;
+        }
+        public static string IDSinhTruong(string maID)
+        {
+            string idNum = "";
+            string idStr = "";
+            foreach(char c in maID)
+            {
+                if (isNumber(c))
+                {
+                    idNum += c + ",";
+                } 
+                else
+                {
+                    idStr += c + ",";
+                }
+            }
+            idNum = idNum.Substring(0, idNum.Length - 1);
+            idStr = idStr.Substring(0, idStr.Length - 1);
+            string[] idNumFull = idNum.Split(',');
+            string[] idStrFull = idStr.Split(',');
+            string idRes = String.Join("", idStrFull) + upCount(String.Join("", idNumFull));
+            return idRes;
+        }
+        private static string upCount(string idNum)
+        {
+            string res = "";
+            int up = Convert.ToInt32(idNum) + 1;
+            if(up < 10)
+            {
+                res = "00" + up;
+            }else if(up > 9 && up < 100)
+            {
+                res = "0" + up;
+            } else if(up > 99)
+            {
+                res = up.ToString();
+            }
+            return res;
+        }
+        public static bool isNumber(char id)
+        {
+            return (Char.IsNumber(id));
+        }
+    }
+}
---
+++ Winform/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TasmaControl;
+using System.Data.SqlClient;
+namespace Winform
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn = TasmaMain.ketnoi(@"DESKTOP-MC\SQLEXPRESS", "QL_BANHANG");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Dictionary<string, object> dlcanthem = new Dictionary<string, object>();
+            //dlcanthem.Add("ID", "KV002");
+            //dlcanthem.Add("NAME", "Nguyễn Trần Lâm");
+            //if (TasmaMain.ThemDuLieu("KHACHVJP", dlcanthem, conn))
+            //{
+            //    MessageBox.Show("Đã Thêm Thành Công " + dlcanthem["ID"]);
+            //}
+            //else MessageBox.Show("Thêm thất bại");
+            //Dictionary<string, object> dlcansua = new Dictionary<string, object>();
+            //dlcansua.Add("TENNV", "Nguyễn Trần Tuyết Mai");
+            //dlcansua.Add("BOPHAN", "Phòng Tính Tèn");
+            //if(TasmaMain.SuaDuLieu("NHANVIEN", dlcansua, "MANV", "NV011", conn))
+            //{
+            //    MessageBox.Show("Đã sửa thành công");
+            //}
+
+            //dataGridView1.DataSource = dt; 
+            //if (TasmaMain.XoaDuLieu("KHACHVsJP", "ID", "KV001", conn))
+            //{
+            //    MessageBox.Show("OKE");
+            //}
+            //else MessageBox.Show("Co loi");
+            //string oke = TasmaMain.IDSinhTruong("MUDA099");
+            //MessageBox.Show(oke);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
